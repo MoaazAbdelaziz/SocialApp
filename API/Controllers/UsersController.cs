8005d6eda4cf5389a2265e33daf48d6f736b7182
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-02-08 17:52:28 +0200

    -Added attributes to methods

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,15 +1,15 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController(DataContext context) : ControllerBase
+
+    public class UsersController(DataContext context) : BaseApiController
     {
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -18,6 +18,7 @@ namespace API.Controllers
             //return ok(users);
         }
 
+        [Authorize]
         [HttpGet("{id:int}")] // api/users/3
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
