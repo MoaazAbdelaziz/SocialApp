10fc22f47da87daea10c5ce859b03ba4880d729b
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-02-10 23:15:43 +0200

    -Refactored the code

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,28 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
+import { RouterOutlet } from '@angular/router';
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
-  title = 'SocialApp';
-  users: any;
+  private accountService = inject(AccountService);
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (res) => (this.users = res),
-      error: (err) => console.log(err),
-      complete: () => console.log('Request Completed'),
-    });
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    var userString = localStorage.getItem('user');
+    if (!userString) return;
+
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 }
