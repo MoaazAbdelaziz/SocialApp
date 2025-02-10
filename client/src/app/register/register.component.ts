5175afa6778d9334fef43db253fd714fbb8dc5c1
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-02-10 23:18:40 +0200

    -Created home component. -Created register component and passed it to home component -Refactored the code.

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  model: any = {};
+  cancelRegister = output<boolean>();
+  private accountService = inject(AccountService);
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.cancel();
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
