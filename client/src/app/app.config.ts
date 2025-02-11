60156dc63573376601b2155733cca38519ecc26f
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-02-11 12:01:06 +0200

    -Provided the toastr

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -11,5 +12,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right',
+    }),
   ],
 };
