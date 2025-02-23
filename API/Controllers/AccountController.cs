24bef2865b3490324d9af83de6e3e97bff7cffef
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-02-23 16:13:20 +0200

    -Must add invlude finction to get the related data of the user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -38,7 +38,9 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+        var user = await context.Users
+        .Include(p => p.Photos)
+        .FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
         if (user is null) return Unauthorized("Invalid username");
 
