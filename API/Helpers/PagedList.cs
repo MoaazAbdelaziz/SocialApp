4bb91e03b92c1414c124e275386c2e0178b9aff7
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-04-03 00:18:13 +0200

    -Fixed spelling

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -7,7 +7,7 @@ public class PagedList<T> : List<T>
     public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
     {
         CurrentPage = pageNumber;
-        ToatalPages = (int)Math.Ceiling(count / (double)pageSize);
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
         PageSize = pageSize;
         TotalCount = count;
         AddRange(items);
@@ -15,7 +15,7 @@ public class PagedList<T> : List<T>
 
     public int CurrentPage { get; set; }
 
-    public int ToatalPages { get; set; }
+    public int TotalPages { get; set; }
 
     public int PageSize { get; set; }
 
