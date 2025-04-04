96abcb80a1929716ce7c1b73fa705a1e821da3ee
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-04-04 14:21:56 +0200

    -Changed claims type -Added a function to get user by user id in the claims principal extension

--- API/Extensions/ClaimsPrincipalExtension.cs
+++ API/Extensions/ClaimsPrincipalExtension.cs
@@ -6,7 +6,12 @@ public static class ClaimsPrincipalExtension
 {
     public static string GetUsername(this ClaimsPrincipal user)
     {
-        var username = user.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("Can not get username from token");
+        var username = user.FindFirstValue(ClaimTypes.Name) ?? throw new Exception("Can not get username from token");
         return username;
+    }
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("Can not get user id from token"));
+        return userId;
     }
 }
