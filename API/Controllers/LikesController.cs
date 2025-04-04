84c157bec9f67eaba941043b8ea8068d0a2bbd10
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-04-04 18:49:44 +0200

    -Finished implementing the like controller

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,52 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class LikesController(ILikesRepository likesRepository) : BaseApiController
+{
+    [HttpPost("{targetUserId:int}")]
+    public async Task<ActionResult> ToggleLike(int targetUserId)
+    {
+        var sourceUserId = User.GetUserId();
+
+        if (sourceUserId == targetUserId) return BadRequest("You can not like yourself");
+
+        var existingLike = await likesRepository.GetUserLike(sourceUserId, targetUserId);
+
+        if (existingLike is null)
+        {
+            var like = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                TargetUserId = targetUserId,
+            };
+            likesRepository.AddLike(like);
+        }
+        else
+        {
+            likesRepository.DeleteLike(existingLike);
+        }
+
+        if (await likesRepository.SaveChanges()) return Ok();
+
+        return BadRequest("Fails to like");
+    }
+
+    [HttpGet("List")]
+    public async Task<ActionResult<IEnumerable<int>>> GetCurrentUserLikesIds()
+    {
+        return Ok(await likesRepository.GetCurrentUserLikeIds(User.GetUserId()));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUserLikes(string predicate)
+    {
+        var users = await likesRepository.GetUserLikes(predicate, User.GetUserId());
+
+        return Ok(users);
+    }
+}
